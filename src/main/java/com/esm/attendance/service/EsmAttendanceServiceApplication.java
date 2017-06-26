72b04731a7f2c831d72d05ffd662d95a836c2657
Durgesh RAI <drai548@gmail.com>
2017-06-26 16:25:04 +0530

    initial commit

---
+++ src/main/java/com/esm/attendance/service/EsmAttendanceServiceApplication.java
@@ -0,0 +1,12 @@
+package com.esm.attendance.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmAttendanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmAttendanceServiceApplication.class, args);
+	}
+}
