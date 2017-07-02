4cae7feafa6c248cc2c58608ac3a32c19a421893
Durgesh RAI <drai548@gmail.com>
2017-07-02 21:51:43 +0530

    renamed the main class

--- src/main/java/com/esm/attendance/service/ESMAttendanceServiceApplication.java
+++ src/main/java/com/esm/attendance/service/ESMAttendanceServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EsmAttendanceServiceApplication {
+public class ESMAttendanceServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EsmAttendanceServiceApplication.class, args);
+		SpringApplication.run(ESMAttendanceServiceApplication.class, args);
 	}
 }
